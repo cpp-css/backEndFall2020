1c8d40f5d6c9b1b63aade0eaf64cab1c0b09ff8f
Daeyoung Hwang <45651823+dyhwang7@users.noreply.github.com>
2020-10-08 12:04:36 -0700

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -4,7 +4,6 @@ from tests.database.receipt import ReceiptTest
 from tests.database.session import SessionTest
 from tests.database.contact import ContactTest
 from tests.database.event import EventTest
-from tests.database.organization import OrganizationTest
 
 
 if __name__ == '__main__':
