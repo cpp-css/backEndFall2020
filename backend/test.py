6b98739e7546bba91cb0828839e43a0e7684b430
Daeyoung Hwang <45651823+dyhwang7@users.noreply.github.com>
2020-10-08 17:39:26 -0700

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,7 @@ import unittest
 from tests.database.notification import NotificationTest
 from tests.database.receipt import ReceiptTest
 from tests.database.session import SessionTest
+from tests.database.organization import OrganizationTest
 
 if __name__ == '__main__':
     unittest.main()
