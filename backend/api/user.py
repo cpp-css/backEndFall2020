5d9f4fc77ff2c100947b54666c5eaa500621c642
Joshua Garde <joshgarde@gmail.com>
2020-10-17 10:56:33 -0700

    Fix signup api

--- backend/api/user.py
+++ backend/api/user.py
@@ -29,15 +29,16 @@ def login(email, password):
 @validate_types(expected={'name': str, 'email': str, 'password': str})
 def signup(name, email, password):
     user = User(email=email, name=name, password=password)
-    session = Session(user_id=user.user_id, expires=datetime.now() + timedelta(days=1))
-    
     db.session.add(user)
+    db.session.commit()
+    
+    session = Session(user_id=user.user_id, expires=datetime.now() + timedelta(days=1))
     db.session.add(session)
     db.session.commit()
     
     return jsonify({
-        success: True,
-        message: '',
+        'success': True,
+        'message': '',
         'session': session.session_id,
         'expires': session.expires
     })
