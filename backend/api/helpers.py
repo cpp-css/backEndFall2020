94ae332a0fb1b39e37530583bacc4faf643250e4
Joshua Garde <joshgarde@gmail.com>
2020-10-22 20:50:13 -0700

    session authentication

--- backend/api/helpers.py
+++ backend/api/helpers.py
@@ -1,11 +1,42 @@
 from functools import wraps
+import re
 from flask import jsonify, request
-from config import DEBUG
+from config import DEBUG, db
+from database.session import Session
+from database.user import User
 
 GENERIC_ERROR = 'Our bad - please contact support for further assistance'
 
 def all_helper():
     return None
+
+def requires_auth(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        raw_auth = request.headers.get('Authorization')
+        if (raw_auth == None):
+            response = {'success': False, 'message': GENERIC_ERROR}
+            if (DEBUG): response['message'] = 'Authorization header missing'
+            return jsonify(response), 400
+        
+        auth = re.search("Bearer (.*)", raw_auth)
+        if (auth == None):
+            response = {'success': False, 'message': GENERIC_ERROR}
+            if (DEBUG): response['message'] = 'Unsupported authorization type'
+            return jsonify(response), 400
+            
+        session_id = auth.group(1)
+        session = db.session.query(Session).filter(Session.session_id == session_id).limit(1).first()
+        
+        if (session == None or session.is_expired()):
+            response = {'success': False, 'message': 'Please login again'}
+            if (DEBUG): response['message'] = 'Session key expired or does not exist'
+            return jsonify(response), 401
+            
+        request.session = session
+        request.user = session.user
+        return func(*args, **kwargs)
+    return wrapper
 
 def requires_json(func):
     @wraps(func)
