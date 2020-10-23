b23f5f5a0f06adb996195abc3b02d566cfcfaf10
Joshua Garde <joshgarde@gmail.com>
2020-10-22 20:52:41 -0700

    fix circular dependencies

--- backend/database/organization.py
+++ backend/database/organization.py
@@ -1,8 +1,6 @@
 from config import db, ma
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
-from database.user import User
-from database.contact import Contact
 
 
 class Organization(db.Model):
